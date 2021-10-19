e46267d1086aaa88a79dea2a821ebf216ee831fc
Sara Ghaedi <saraghaedi75@gmail.com>
2021-10-19 17:42:39 +0200

    set up heroku for deployment

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const spaceRouter = require("./routers/space");
 const eventRouter = require("./routers/event");
 
 const app = express();
+app.use(corsMiddleWare());
 
 app.use(loggerMiddleWare("dev"));
 app.use(bodyParserMiddleWare);
-app.use(corsMiddleWare());
 if (process.env.DELAY) {
   app.use((req, res, next) => {
     setTimeout(() => next(), parseInt(process.env.DELAY));
